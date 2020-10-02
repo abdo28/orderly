d559541d5a4046717a4a0d072fa060ec12bcc1c5
abdo28 <abedalkhaleq28@gmail.com>
2020-10-02 09:26:11 +0300

    set up

---
+++ app.js
@@ -0,0 +1,62 @@
+var express = require("express");
+var bodyParser  = require("body-parser");
+var mongoose = require("mongoose");
+
+app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/orderly_v1" );
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+ });
+
+ var Campground = mongoose.model("Campground", campgroundSchema);
+ 
+/* Campground.create(
+     {
+         name: "سارة الجعبري", 
+         image: "ما بعرف شو دخلكم",
+         description: "11122 شارع أحمد السعيدي"
+         
+     },
+     function(err, campground){
+      if(err){
+          console.log(err);
+      } else {
+          console.log("NEWLY CREATED CAMPGROUND: ");
+          console.log(campground);
+      }
+}); 
+
+app.get("/", function(req,res){
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+           res.render("index",{campgrounds:allCampgrounds});
+        }
+     });
+}); */
+
+
+//mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb
+//const http = require('http');
+
+
+
+/*const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+*/
+app.listen(3000, '127.0.0.1', () => {
+  console.log(`Server running at http://${'127.0.0.1'}:${3000}/`);
+});
