67c6f5d88f6556d60c811adfa61539deb0e9e141
abdo28 <abedalkhaleq28@gmail.com>
2020-12-16 15:22:31 +0200

    update math

--- app.js
+++ app.js
@@ -632,21 +632,98 @@ app.get("/math",isLoggedIn, async function(req, res){
               console.log(err);
             } else {
                //await res.render("sale", {jomleh: foundWOrders, tawseel:foundOrders, tasleem:foundTasleem});
-               let netFinalCost = 0;
+               warehouse.find({userOwner: res.locals.currentUser}, async function(err, allWarehouseItems){
+                if(err){
+                    console.log(err);
+                } else {
+                
+            
+               let netRev = 0;
+               let itemrev =0;
               for(let i=0; i<foundOrders.length; i++){
-                netFinalCost+=foundOrders[i].finalCost;
+                for(let j=0;j<foundOrders[i].items.length;j++){
+                  netRev+= (foundOrders[i].items[j].priceForEachOne- foundOrders[i].items[j].originalPriceForEachOne)*foundOrders[i].items[j].amount;
+                }
+                netRev-=foundOrders[i].discount;
               
               }
               for(let i=0; i<foundWOrders.length; i++){
-                netFinalCost+=foundWOrders[i].finalCost;
-              
+                for(let j=0;j<foundWOrders[i].items.length;j++){
+                  netRev+= (foundWOrders[i].items[j].priceForEachOne- foundWOrders[i].items[j].originalPriceForEachOne)*foundWOrders[i].items[j].amount;
+                }     
+                netRev-=foundWOrders[i].discount;         
               }
               for(let i=0; i<foundTasleem.length; i++){
-                netFinalCost+=foundTasleem[i].finalCost;
+                for(let j=0;j<foundTasleem[i].items.length;j++){
+                  netRev+= (foundTasleem[i].items[j].priceForEachOne- foundTasleem[i].items[j].originalPriceForEachOne)*foundTasleem[i].items[j].amount;
+                }
+                netRev-=foundTasleem[i].discount;
 
               }
+              await res.render("math", {rev:netRev, items:allWarehouseItems,itemrev:itemrev});
+            }
+            });
+            }
+          });
+        }
+      });
+    }
+  });
+}); 
+
+app.post("/math/netrev",isLoggedIn, async function(req, res){
+  await order.find({recivedByCustomer:true, isTawseel:true, userOwner: res.locals.currentUser }).populate("items").exec( async function(err, foundOrders){
+    if(err){
+      console.log(err);
+    } else {
+      await order.find({recivedByCustomer:true,  extraForDelivery:0, userOwner: res.locals.currentUser}).populate("items").exec( async function(err, foundTasleem){
+        if(err){
+          console.log(err);
+        } else {
+          await wholesale.find({FullyPay:true, userOwner: res.locals.currentUser}).populate("items").exec( async function(err, foundWOrders){
+            if(err){
+              console.log(err);
+            } else {
+               warehouse.find({userOwner: res.locals.currentUser}, async function(err, allWarehouseItems){
+                if(err){
+                    console.log(err);
+                } else {
+                
+            
+               let netRev = 0;
+               let itemrev=0;
+              for(let i=0; i<foundOrders.length; i++){
+                for(let j=0;j<foundOrders[i].items.length;j++){
+                  if(foundOrders[i].items[j].type==req.params.type)
+                    itemrev+= (foundOrders[i].items[j].priceForEachOne- foundOrders[i].items[j].originalPriceForEachOne)*foundOrders[i].items[j].amount;
+                    netRev+= (foundOrders[i].items[j].priceForEachOne- foundOrders[i].items[j].originalPriceForEachOne)*foundOrders[i].items[j].amount;
+
+                }
+                netRev-=foundOrders[i].discount;
               
-              console.log(netFinalCost);
+              }
+              for(let i=0; i<foundWOrders.length; i++){
+                for(let j=0;j<foundWOrders[i].items.length;j++){
+                  if(foundWOrders[i].items[j].type==req.params.type)
+                  itemrev+= (foundWOrders[i].items[j].priceForEachOne- foundWOrders[i].items[j].originalPriceForEachOne)*foundWOrders[i].items[j].amount;
+                  netRev+= (foundWOrders[i].items[j].priceForEachOne- foundWOrders[i].items[j].originalPriceForEachOne)*foundWOrders[i].items[j].amount;
+
+                }     
+                netRev-=foundWOrders[i].discount;         
+              }
+              for(let i=0; i<foundTasleem.length; i++){
+                for(let j=0;j<foundTasleem[i].items.length;j++){
+                  if(foundTasleem[i].items[j].type==req.params.type)
+                  itemrev+= (foundTasleem[i].items[j].priceForEachOne- foundTasleem[i].items[j].originalPriceForEachOne)*foundTasleem[i].items[j].amount;
+                  netRev+= (foundTasleem[i].items[j].priceForEachOne- foundTasleem[i].items[j].originalPriceForEachOne)*foundTasleem[i].items[j].amount;
+
+                }
+                netRev-=foundTasleem[i].discount;
+
+              }
+             await res.render("math", {rev:netRev, items:allWarehouseItems,itemrev:itemrev});
+            }
+            });
             }
           });
         }
